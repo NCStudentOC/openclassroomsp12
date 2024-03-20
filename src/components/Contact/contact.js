78db3bb6a7cbeb62f9bbfe990f4057843dd5a5b6
NCStudentOC <moulin.y@proton.me>
2024-03-20 11:24:22 +0100

    version deployée

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,18 +1,10 @@
 import React, { useState } from 'react';
 import Message from '../Message/Message';
 
-
-
 export default function Contact() {
-    const [formState, setFormState] = useState({
-        fname: '',
-        lname: '',
-        email: '',
-        message: ''
-    });
+    const [formState, setFormState] = useState({ fname: '', lname: '', email: '', message: '' });
     const [message, setMessage] = useState({ type: '', text: '' });
     const [isSubmitted, setIsSubmitted] = useState(false);
-
 
     const handleChange = (e) => {
         setFormState({
@@ -21,35 +13,18 @@ export default function Contact() {
         });
     };
 
-
-    const validateEmail = (email) => {
-        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
+    const handleClick = (e) => {
+        if (!formState.fname || !formState.lname || !formState.email || !formState.message) {
+            e.preventDefault();
+            setMessage({ type: 'error', text: 'Veuillez remplir  les champs email, nom, prénom.' });
+        } else if (!/\S+@\S+\.\S+/.test(formState.email)) {
+            e.preventDefault();
+            setMessage({ type: 'error', text: 'Veuillez entrer un email valide.' });
+        } else {
+            setIsSubmitted(true);
+            setMessage({ type: 'success', text: 'Votre message a été envoyé avec succès.' });
+        }
     };
-
-
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     if (formState.fname === '' || formState.lname === '' || formState.email === '') {
-    //         setMessage({ type: 'error', text: 'Le prénom, le nom et l\'e-mail sont obligatoires' });
-    //     } else if (formState.email !== '' && !validateEmail(formState.email)) {
-    //         setMessage({ type: 'error', text: 'Veuillez entrer une adresse e-mail valide' });
-    //     } else {
-    //         console.log(formState);
-    //         // Si le formulaire est soumis avec succès, définissez le message de succès
-    //         setMessage({ type: 'success', text: 'Le message a été envoyé avec succès !' });
-
-    //         setFormState({
-    //             fname: '',
-    //             lname: '',
-    //             email: '',
-    //             message: ''
-    //         });
-    //         setIsSubmitted(true);
-    //     }
-    // };
-
-
 
     return (
         <div id="point_contact">
@@ -62,12 +37,12 @@ export default function Contact() {
                     <input type="hidden" name="form-name" value="contact" />
 
                     <label>
-                        Prénom :
-                        <input type="text" name="fname" value={formState.fname} onChange={handleChange} />
+                        Nom :
+                        <input type="text" name="fname" onChange={handleChange} />
                     </label>
                     <label>
-                        Nom :
-                        <input type="text" name="lname" value={formState.lname} onChange={handleChange} />
+                        Prénon :
+                        <input type="text" name="lname" onChange={handleChange} />
                     </label>
                     <label>
                         E-mail :
@@ -80,9 +55,9 @@ export default function Contact() {
                     </label>
                     <label>
                         Message:
-                        <textarea name="message" value={formState.message} onChange={handleChange} rows="5" cols="50" />
+                        <textarea name="message" onChange={handleChange} rows="5" cols="50" />
                     </label>
-                    <input type="submit" value="Envoyer" className={`button_submit ${isSubmitted ? 'success' : ''}`} />
+                    <input type="submit" value="Envoyer" onClick={handleClick} className={`button_submit ${isSubmitted ? 'success' : ''}`} />
                 </form>
                 {message.text && <Message message={message} clearMessage={() => setMessage({ type: '', text: '' })} />}
             </div>
